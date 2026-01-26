6610394dd6426da4d64726997a3d54c447182c94
Serlis Maldonado <serlismaldonado@github.com>
2026-01-26 07:21:04 +0000

    Try: Import betterAuth directly from package

--- convex/convex.ts
+++ convex/convex.ts
@@ -1,10 +1,8 @@
 import { defineApp } from "convex/server";
-import betterAuth from "@convex-dev/better-auth/convex.config";
+import betterAuth from "@convex-dev/better-auth";
 
 const app = defineApp();
 
-app.use(betterAuth, {
-  componentDefinitionPath: "/convex/convex.ts",
-});
+app.use(betterAuth);
 
 export default app;
